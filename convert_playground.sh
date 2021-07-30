5e1490abad14d19bdfbc2211446c91acddbc1760
Jan Romann <jan.romann@uni-bremen.de>
2021-07-30 02:08:45 +0200

    Add CI workflow for SDF to WoT conversion

---
+++ convert_playground.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone https://github.com/one-data-model/playground.git
+rm -fR playground/tm
+mkdir -p output/tm
+for filename in playground/sdfObject/*.sdf.json; do
+    echo "Converting $filename ..."
+    sdf-wot-converter --from-sdf "$filename" --to-tm "output/tm/$(basename "$filename" .sdf.json).tm.json"
+done
