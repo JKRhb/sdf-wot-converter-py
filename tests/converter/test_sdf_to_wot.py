19d024e6af63f4b872c60b881362e47b0ff2f0e6
Jan Romann <jan.romann@uni-bremen.de>
2021-07-31 20:49:34 +0200

    SDF -> WoT: Add test case for failing URL sdfRefs

--- tests/converter/test_sdf_to_wot.py
+++ tests/converter/test_sdf_to_wot.py
@@ -411,3 +411,25 @@ def test_sdf_tm_unparsabable_sdf_ref():
         perform_conversion_test(input, expected_result, sdf_tm_helper)
 
     assert str(e_info.value) == "sdfRef bla/sdfProperty/bar could not be resolved"
+
+
+def test_sdf_tm_failing_URL_sdf_ref():
+    input = {
+        "namespace": {
+            "bla": "https://example.org"
+        },
+        "sdfProperty": {
+            "foo": {
+                "sdfRef": "bla:/sdfProperty/bar"
+            },
+        },
+    }
+
+    expected_result = None
+
+    with pytest.raises(Exception) as e_info:
+        perform_conversion_test(input, expected_result, sdf_tm_helper)
+
+    error_message = "No valid SDF model could be retrieved from https://example.org"
+
+    assert str(e_info.value) == error_message
