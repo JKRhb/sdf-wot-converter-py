5a8b9d93a4586310d57705677c067ac625b199a7
Jan Romann <jan.romann@uni-bremen.de>
2021-08-03 16:33:47 +0200

    Add simple tests for CLI functions

--- tests/converter/test_cli.py
+++ tests/converter/test_cli.py
@@ -1,5 +1,7 @@
 from converter import parse_arguments
-
+from converter import convert_sdf_to_wot_tm_from_path
+from converter import convert_wot_tm_to_sdf_from_path
+import os
 
 def test_parse_arguments():
     args1 = ["--from-sdf", "foo", "--to-tm", "bar"]
@@ -9,3 +11,23 @@ def test_parse_arguments():
     args2 = ["--from-tm", "foo", "--to-sdf", "bar"]
     parsed_args2 = parse_arguments(args2)
     assert parsed_args2.from_tm == "foo" and parsed_args2.to_sdf == "bar"
+
+def make_test_output_dir():
+    try:
+        os.mkdir("test_output")
+    except FileExistsError:
+        pass
+
+
+def test_sdf_example_conversion():
+    # TODO: Check for correct test output
+    make_test_output_dir()
+    convert_sdf_to_wot_tm_from_path(
+        "examples/sdf/example.sdf.json", "test_output/blah.tm.json"
+    )
+
+
+def test_wot_example_conversion():
+    # TODO: Check for correct test output
+    make_test_output_dir()
+    convert_wot_tm_to_sdf_from_path("examples/wot/example.tm.json", "test_output/blah.sdf.json")
