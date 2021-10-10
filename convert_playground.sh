39d76a2a606ef77b16ee99c36dcecda22b662d85
Jan Romann <jan.romann@uni-bremen.de>
2021-10-10 22:50:04 +0200

    chore: add origin URL to converted playground models

--- convert_playground.sh
+++ convert_playground.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+origin=https://raw.githubusercontent.com/one-data-model/playground/master
 git clone https://github.com/one-data-model/playground.git
 rm -fR playground/tm
 mkdir -p output/tm
-for filename in playground/sdfObject/*.sdf.json; do
+cd playground
+for filename in sdfObject/*.sdf.json; do
     echo "Converting $filename ..."
-    sdf-wot-converter --from-sdf "$filename" --to-tm "output/tm/$(basename "$filename" .sdf.json).tm.jsonld"
+    sdf-wot-converter --from-sdf "$filename" --to-tm "../output/tm/$(basename "$filename" .sdf.json).tm.jsonld" --origin-url $origin/$filename
 done
