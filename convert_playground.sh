be1a5d4271290d02b75e0feb3a2f9f5eedd846e2
Jan Romann <jan.romann@uni-bremen.de>
2022-06-14 16:57:05 +0200

    fix: update convert_playground script

--- convert_playground.sh
+++ convert_playground.sh
@@ -4,7 +4,7 @@ git clone https://github.com/one-data-model/playground.git
 rm -fR playground/tm
 mkdir -p output/tm
 cd playground
-for filename in sdfObject/*.sdf.json; do
+for filename in sdfObject/sdfobject*.sdf.json; do
     echo "Converting $filename ..."
-    sdf-wot-converter sdf-to-tm -i "$filename" -o "../output/tm/$(basename "$filename" .sdf.json).tm.json" --origin-url $origin/$filename
+    sdf-wot-converter sdf-to-tm -i "$filename" -o "../output/tm/$(basename "$filename" .sdf.json).tm.jsonld" --origin-url $origin/$filename
 done
