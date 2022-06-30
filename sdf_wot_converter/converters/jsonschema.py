9a276a5655a1412c4200ff55896b4d9b2e7c89ac
Jan Romann <jan.romann@uni-bremen.de>
2022-06-30 21:22:22 +0200

    fix: fix mapping of exclusiveMinimum and exclusiveMaximum

--- sdf_wot_converter/converters/jsonschema.py
+++ sdf_wot_converter/converters/jsonschema.py
@@ -27,30 +27,6 @@ def map_common_json_schema_fields(
     map_required(source_definition, target_definition, mapped_fields)
     map_format(source_definition, target_definition, mapped_fields)
     map_pattern(source_definition, target_definition, mapped_fields)
-    map_exclusive_minimum(source_definition, target_definition, mapped_fields)
-    map_exclusive_maximum(source_definition, target_definition, mapped_fields)
-
-
-def map_exclusive_maximum(
-    source_definition, target_definition, mapped_fields: List[str]
-):
-    map_common_field(
-        source_definition,
-        target_definition,
-        "exclusiveMaximum",
-        mapped_fields=mapped_fields,
-    )
-
-
-def map_exclusive_minimum(
-    source_definition, target_definition, mapped_fields: List[str]
-):
-    map_common_field(
-        source_definition,
-        target_definition,
-        "exclusiveMinimum",
-        mapped_fields=mapped_fields,
-    )
 
 
 def map_pattern(source_definition, target_definition, mapped_fields: List[str]):
